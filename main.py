973194dee55e6e14a6eb1c61857c270f908a5fbf
Dhinesh KS <dhinaksd@gmail.com>
2021-11-26 17:46:40 +0530

    PDF merging

--- main.py
+++ main.py
@@ -2,42 +2,52 @@ import asyncio
 import io
 from pyppeteer import launch
 from PyPDF2 import PdfFileMerger, PdfFileReader
+import time
 
 # Configurations
 chromiumPath = r"C:\Users\dhinesh.ks\Desktop\.local-chromium\Win64-884014\chrome-win\chrome.exe"
 viewPort = {'width': 2000, 'height': 1200}
 url = 'http://127.0.0.1:5500/html/index4.html'
-urlConfig = {'timeout': 90000, 'waitUntil': 'domcontentloaded'}
-pdfConfig = {'path': 'page.pdf', 'width': 1900, 'height': 1200,
+urlConfig = {'timeout': 90000, 'waitUntil': ['load', 'domcontentloaded', 'networkidle0']}
+pdfConfig = {'width': 1900, 'height': 1200,
              'margin': {'top': 50, 'left': 50, 'right': 50, 'bottom': 50}}
+# 'path': 'page.pdf'
+pages = [];
 
 
 # PDF Merging
-def mergePDF(bytes):
+def mergePDF():
     merger = PdfFileMerger()
-    merger.append(PdfFileReader(io.BytesIO(bytes)))
-    merger.write("document-output.pdf")
+    if len(pages) > 0:
+        for item in pages:
+            merger.append(PdfFileReader(io.BytesIO(item)))
+        merger.write("document-output.pdf")
+    else:
+        print("No pages found!")
 
 
 # HTML to PDF
-async def htmlToPdf(name):
+async def htmlToPdf():
     browser = await launch(headless=True, executablePath=chromiumPath)
     page = await browser.newPage()
     await page.setViewport(viewPort)
-    await page.goto(url)  # can take urlconfig as second arg
-    doc = await page.pdf(
-        {'path': name, 'width': 1900, 'height': 1200, 'margin': {'top': 50, 'left': 50, 'right': 50, 'bottom': 50}})
-    mergePDF(doc)
+    await page.goto(url, urlConfig)  # can take urlconfig as second arg
+    doc = await page.pdf(pdfConfig)
+    pages.append(doc)
     await browser.close()
 
 
 async def main():
     tasks = []
     print("Process Started")
+    start_time = time.perf_counter()
     for x in range(5):
-        tasks.append(asyncio.create_task(htmlToPdf(f"test{x}.pdf")))
-    L = await asyncio.gather(*tasks)
+        tasks.append(asyncio.create_task(htmlToPdf()))
+    await asyncio.gather(*tasks)
+    mergePDF()
     print("Process Completed")
+    end_time = time.perf_counter()
+    print("Total duration in seconds -", end_time - start_time)
 
 
 asyncio.get_event_loop().run_until_complete(main())
