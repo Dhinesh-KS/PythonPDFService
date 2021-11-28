e34e8fbe1e8fc2b0da8bba0b062767a180e03caf
Dhinesh KS <dhinaksd@gmail.com>
2021-11-28 21:41:10 +0530

    Parallel processing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 chromiumPath = r"C:\Users\dhinesh.ks\Desktop\.local-chromium\Win64-884014\chrome-win\chrome.exe"
 viewPort = {'width': 2000, 'height': 1200}
 url = 'http://127.0.0.1:5500/html/index4.html'
-urlConfig = {'timeout': 90000, 'waitUntil': ['load', 'domcontentloaded', 'networkidle0']}
+urlConfig = {'timeout': 9000000, 'waitUntil': ['load', 'domcontentloaded', 'networkidle0']}
 pdfConfig = {'width': 1900, 'height': 1200,
              'margin': {'top': 50, 'left': 50, 'right': 50, 'bottom': 50}}
 # 'path': 'page.pdf'
@@ -26,28 +26,32 @@ def mergePDF():
         print("No pages found!")
 
 
-# HTML to PDF
-async def htmlToPdf():
-    browser = await launch(headless=True, executablePath=chromiumPath)
+async def fetchPages(browser):
     page = await browser.newPage()
     await page.setViewport(viewPort)
     await page.goto(url, urlConfig)  # can take urlconfig as second arg
     doc = await page.pdf(pdfConfig)
     pages.append(doc)
+
+
+# HTML to PDF
+async def htmlToPdf():
+    tasks = []
+    browser = await launch(headless=True, executablePath=chromiumPath)
+    for x in range(2):
+        tasks.append(asyncio.create_task(fetchPages(browser)))
+    await asyncio.gather(*tasks)
+    mergePDF()
     await browser.close()
 
 
-async def main():
-    tasks = []
+def main():
     print("Process Started")
     start_time = time.perf_counter()
-    for x in range(5):
-        tasks.append(asyncio.create_task(htmlToPdf()))
-    await asyncio.gather(*tasks)
-    mergePDF()
+    asyncio.get_event_loop().run_until_complete(htmlToPdf())
     print("Process Completed")
     end_time = time.perf_counter()
     print("Total duration in seconds -", end_time - start_time)
 
 
-asyncio.get_event_loop().run_until_complete(main())
+main()
